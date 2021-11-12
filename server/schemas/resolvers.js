0582124e0160a2e9e5fe07974effda8c3f4d9852
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-12 17:59:32 -0500

    fixed me query and added savebook and removebook mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,8 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
-        me: async (parent, { username }) => {
-            return User.findOne({ username }).populate("savedBooks")
+        me: async (parent, _, context) => {
+            return User.findOne({ _id: context.user._id }).populate("savedBooks")
         }
     },
     Mutation: {
@@ -31,8 +31,39 @@ const resolvers = {
 
             return { token, user };
         },
-        
-    }
-}
+        saveBook: async (parent, { authors, description, title, bookId, image, link }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(   
+                    { _id: context.user },
+                    {
+                        $addToSet: {
+                            savedBooks: { authors, description, title, bookId, image, link },
+                        },
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    {
+                        $pull: {
+                            savedBooks: { bookId: bookId }
+                        },
+                    },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        },
+    },
+};
 
 module.exports = resolvers;
