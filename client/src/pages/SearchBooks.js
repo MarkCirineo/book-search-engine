1cfabb8a5d9a7be6fee6f7154eb8954ba3914782
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-14 22:32:23 -0500

    refactored to use GraphQL API

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
+import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from "../utils/queries"
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -19,6 +22,25 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
+
+  const [saveBook, { error }] = useMutation(SAVE_BOOK, {
+    update(cache, { data: { saveBook } }) {
+      try {
+        const { me } = cache.readQuery({ query: GET_ME });
+        
+        console.log(me);
+
+        const savedBooks = me.savedBooks;
+
+        cache.writeQuery({
+          query: GET_ME,
+          data: { savedBooks: [saveBook, ...savedBooks] },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  })
 
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
@@ -57,19 +79,11 @@ const SearchBooks = () => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      await saveBook({
+        variables: bookToSave
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
