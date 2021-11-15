288876d0d64cb877ca4ab6c1dd9d48bfb88254f6
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-11-14 22:33:57 -0500

    link no longer required in saveBook mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,10 @@ const { signToken } = require("../utils/auth");
 const resolvers = {
     Query: {
         me: async (parent, _, context) => {
-            return User.findOne({ _id: context.user._id }).populate("savedBooks")
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate("savedBooks")
+            }
+            throw new AuthenticationError("You must be logged in!");
         }
     },
     Mutation: {
@@ -34,7 +37,7 @@ const resolvers = {
         saveBook: async (parent, { authors, description, title, bookId, image, link }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(   
-                    { _id: context.user },
+                    { _id: context.user._id },
                     {
                         $addToSet: {
                             savedBooks: { authors, description, title, bookId, image, link },
